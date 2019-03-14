e85a25775dddfe52158165b0720f87654a3e1549
Cristofer Casas <cristofer32513@gmail.com>
2019-03-14 11:24:55 -0600

    Se agrego la clase BlockingBuffer.

---
+++ src/BlockingBuffer.java
@@ -0,0 +1,29 @@
+// Fig. 23.14: BlockingBuffer.java
+// Creating a synchronized buffer using an ArrayBlockingQueue.
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class BlockingBuffer implements Buffer {
+	
+	private final ArrayBlockingQueue<Integer> buffer; // shared buffer
+	
+	public BlockingBuffer() {
+		buffer = new ArrayBlockingQueue<Integer>(1);
+	}
+
+	// place value into buffer
+	public void blockingPut(int value) throws InterruptedException {
+		buffer.put(value); // place value in buffer
+		System.out.printf("%s%2d\t%s%d%n", "Productor escribe      ", value,
+				"Buffer cells occupied: ", buffer.size());
+	}
+
+	// return value from buffer
+	public int blockingGet() throws InterruptedException {
+		int readValue = buffer.take(); // remove value from buffer
+		System.out.printf("%s %2d\t%s%d%n", "Consumidor lee        ",
+				readValue, "Buffer cells occupied: ", buffer.size());
+
+		return readValue;
+	}
+} // end class BlockingBuffer
